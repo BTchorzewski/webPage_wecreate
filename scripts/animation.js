e77dcc1e8768c5c4d5d5c3961e657cfdf3395e82
BTchorzewski <tchorzewskibartosz@gmail.com>
2018-10-25 22:15:58 +0200

    programing animation

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,19 +1,34 @@
 document.addEventListener("DOMContentLoaded", function(){
-    var clickMenuElement = document.querySelector(".menu-clicker");
-    var navigationBar = document.querySelector(".column-menu");
+    var navigation__icon = document.querySelector(".navigation");
+    var navigation__link = document.querySelectorAll(".navigation__link");
     
-    
-    console.log(navigationBar.style);
-
-    function animateNavBar() {
-        navigationBar.style.width = '100%';
-        
+    console.table(navigation__link[0].getAttribute('class'));
+    function animated__navigation() {
+        if(navigation__link[0].classList.contains('navigation__link--active') == false && navigation__link[0].classList.contains('navigation__link--unactive') == false) {
+            navigation__link.forEach(function(el) {
+                el.classList.add('navigation__link--active');
+            });
+            console.table(navigation__link[0].getAttribute('class'));
+        } 
+        else if(navigation__link[0].classList.contains('navigation__link--active')) {
+            navigation__link.forEach(function(el){
+                el.classList.replace('navigation__link--active', 'navigation__link--unactive');
+            });
+            console.table(navigation__link[0].getAttribute('class'));
+        } 
+        else if(navigation__link[0].classList.contains('navigation__link--unactive')) {
+            navigation__link.forEach(function(el){
+                el.classList.replace('navigation__link--unactive', 'navigation__link--active');
+            });
+            console.table(navigation__link[0].getAttribute('class'));
+        }
     }
+    
 
-    clickMenuElement.addEventListener("click", animateNavBar);
 
+    navigation__icon.addEventListener("click", animated__navigation);
+    
 });
-
 
 
 
